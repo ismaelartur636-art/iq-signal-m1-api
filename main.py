d793ef59624f3cf3382c7a81f6e3d7fd0ed040d9
ismaelartur636-art <ismaelartur636@gmail.com>
2026-09-11 10:28:11 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1566,4 +1566,4 @@ async def result(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000"))
